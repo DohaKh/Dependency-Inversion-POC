c8598f703255f233e669ee0a6f6c06f0cd3c7b84
Doha Khaled <Dkhaled@integrant.com>
2020-09-02 09:50:14 +0200

    first commit

---
+++ CountingFemaleManagersWithDIP/EmployeeStatistics.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CountingFemaleManagersWithDIP
+{
+    public class EmployeeStatistics
+    {
+        private readonly IEmployeeSearchable _emp;
+        public EmployeeStatistics(IEmployeeSearchable emp)
+        {
+            _emp = emp;
+        }
+        public int CountFemaleManagers() => _emp.GetEmployeesByGenderAndPosition(Gender.Female, Position.Manager).Count();
+    }
+}
---
+++ DependencyInjectionWithoutDIP/EmployeeManager.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CountingFemaleManagersWithoutDIP
+{
+    /*
+     * low-level class which keeps (in a simplified way) track of our employees.
+     * With this kind of structure in our EmployeeManager class, we can’t make use of the 
+     * _employess list in the EmployeeStatistics class, 
+     * so the obvious solution would be to expose that private list.
+     */
+    public class EmployeeManager
+    {
+        private readonly List<Employee> _employees;
+
+        public EmployeeManager()
+        {
+            _employees = new List<Employee>();
+        }
+        public void AddEmployee(Employee employee)
+        {
+            _employees.Add(employee);
+        }
+        public List<Employee> Employees => _employees;
+    }
+}
